3a3e93f1df4d1a55b9adcaa72b05513be28b6d0b
ivansugi <ivan@wirekom.co.id>
2015-08-06 21:18:38 +0700

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,3 @@
+from oprex import *
+import parsetab import *
+
---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+setup(
+    # Application name:
+    name="oprex",
+
+    # Version number (initial):
+    version="0.1.0",
+
+    # Application author details:
+    author="Ron Panduwana",
+    author_email="panduwana@gmail.com",
+
+    # Packages
+    packages=["app"],
+
+    # Include additional files into the package
+    include_package_data=True,
+
+    # Details
+    url="http://pypi.python.org/pypi/oprex/",
+
+    #
+    # license="LICENSE.txt",
+    description="parse some code",
+
+    # long_description=open("README.txt").read(),
+
+    # Dependent packages (distributions)
+    install_requires=[
+        "argparse>=1.2.1",
+		"ply>=3.4",
+		"regex>=2014.12.24",
+    ],
+)
